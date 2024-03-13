d5011aef28612212eab7921809ca0463c0a7e57b
wswenyue <wswenyue@163.com>
2024-03-13 11:15:26 +0800

    MOD: docs

--- .github/data/aklog.rb
+++ .github/data/aklog.rb
@@ -4,9 +4,7 @@
 class Aklog < Formula
   desc "Android developer's Swiss Army Knife for Log"
   homepage "https://github.com/wswenyue/aklog"
-  #https://github.com/wswenyue/aklog/archive/v5.0.4.tar.gz
   url "#_url_#"
-  #699b3d936f1456d9893c5f34850f959b5b3492119eff518c6366d4935c7449a2
   sha256 "#_sha256_#"
 
 
